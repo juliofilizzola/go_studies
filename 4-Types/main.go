b7e5943bd0bb5a0f54294d963834e169e0d15910
julio_filizzola <juliofilizzola@hotmail.com>
2022-12-03 18:38:38 -0300

    feat: type int e uint

---
+++ 4-Types/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var number1 int64 = 1000000000000000
+	fmt.Println(number1)
+	var number2 uint64 = 111111111111111
+	fmt.Println(number2)
+
+	// alias
+	// int32  RUNE
+
+	var number3 rune = 12345
+	fmt.Println(number3)
+
+}
