b24d9ca664ceb6c0ffdad530a4b106752a0dfe71
julio_filizzola <juliofilizzola@hotmail.com>
2022-12-03 10:28:08 -0300

    Feat: create module from variables

--- 3-Variables/main.go
+++ 3-Variables/main.go
@@ -7,4 +7,13 @@ func main() {
 	var2 := "var 2"
 	fmt.Printf(var1)
 	fmt.Printf(var2)
+	var (
+		var3 string
+		var4 string
+	)
+
+	var3 = "Hello"
+	var4 = "Stranger"
+	fmt.Println(var3)
+	fmt.Println(var4)
 }
