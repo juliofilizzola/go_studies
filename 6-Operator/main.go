98a6d83f9f171a1ce131b70b316649c127d54fb2
julio_filizzola <juliofilizzola@hotmail.com>
2022-12-04 14:06:28 -0300

    Feat: operator relational

--- 6-Operator/main.go
+++ 6-Operator/main.go
@@ -13,5 +13,13 @@ func main() {
 
 	const var1 string = "string"
 	var2 := "string 2"
-	print(var1, var2)
+	println(var1, var2)
+
+	max := 1 >= 2
+	min := 2 <= 1
+	igual := 2 == 2
+
+	println(max)
+	println(min)
+	println(igual)
 }
