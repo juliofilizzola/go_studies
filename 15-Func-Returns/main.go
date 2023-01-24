3e0298da3282e6d398ec8c1712fdcf49177d9970
julio_filizzola <juliofilizzola@hotmail.com>
2023-01-24 15:56:15 -0300

    Feat: example func returns

--- 15-Func-Returns/main.go
+++ 15-Func-Returns/main.go
@@ -1,1 +1,18 @@
 package main
+
+import "fmt"
+
+func calMath(n1, n2 int) (soma int, subtract int) {
+	soma = n1 + n2
+	subtract = n1 - n2
+	return
+}
+
+func main() {
+	fmt.Println("Func returns")
+
+	soma, sub := calMath(1, 2)
+
+	fmt.Println(soma, "soma")
+	fmt.Println(sub, "sub")
+}
