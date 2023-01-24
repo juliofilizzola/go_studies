683a3ec27cbf0f1b11305924281e138a5b2a1afd
julio_filizzola <juliofilizzola@hotmail.com>
2023-01-24 11:17:09 -0300

    Feat: example from loop for

--- 14-Loop/main.go
+++ 14-Loop/main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Loops")
+	timeVar := 0
+
+	for timeVar < 10 {
+		timeVar++
+		fmt.Println("incrementando")
+		time.Sleep(time.Second)
+	}
+
+	fmt.Println(timeVar)
+
+}
