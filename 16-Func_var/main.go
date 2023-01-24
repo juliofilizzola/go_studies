da7aadb47bb0ec20eee0a134b9e4d6e83be30895
julio_filizzola <juliofilizzola@hotmail.com>
2023-01-24 17:19:23 -0300

    Feat: final example

--- 16-Func_var/main.go
+++ 16-Func_var/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"16-Func_var/func_var"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Func var")
+	var1 := func_var.Soma(2, 3, 53, 1223, 323, 23)
+
+	fmt.Println(var1)
 }
