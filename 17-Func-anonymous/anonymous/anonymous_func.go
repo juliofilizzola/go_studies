7c9052134cfeab549787cc1ba90fbfc342082dcc
julio_filizzola <juliofilizzola@hotmail.com>
2023-01-24 17:31:02 -0300

    Feat: fineshed anonymous func

--- 17-Func-anonymous/anonymous/anonymous_func.go
+++ 17-Func-anonymous/anonymous/anonymous_func.go
@@ -1,3 +1,13 @@
 package anonymous
 
-func Anonymous() {}
+import (
+	"fmt"
+)
+
+func Anonymous() {
+	fmt.Println("test")
+
+	func(text string) {
+		fmt.Println(text)
+	}("Passando o parametro")
+}
