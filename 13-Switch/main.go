55b7119b4745f1e5131ac5888d0ad0b99f78ddb7
julio_filizzola <juliofilizzola@hotmail.com>
2023-01-24 09:00:48 -0300

    Feat: create func dayWeek from example

--- 13-Switch/main.go
+++ 13-Switch/main.go
@@ -2,6 +2,31 @@ package main
 
 import "fmt"
 
+func dayWeek(numberDay int) string {
+	switch numberDay {
+	case 1:
+		return "Doming"
+	case 2:
+		return "Segunda-Feira"
+	case 3:
+		return "Terça-Feira"
+	case 4:
+		return "Quarta-Feira"
+	case 5:
+		return "Quinta-Feira"
+	case 6:
+		return "Sexta-Feira"
+	case 7:
+		return "Sabádo"
+	default:
+		return "Numero invalido"
+	}
+}
+
 func main() {
 	fmt.Println("Switch")
+
+	resolve := dayWeek(2)
+
+	fmt.Println(resolve)
 }
