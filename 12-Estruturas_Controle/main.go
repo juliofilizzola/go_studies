7b7ef99c0ac045724fe4e8b3c498823ec00d4c41
julio_filizzola <juliofilizzola@hotmail.com>
2023-01-24 08:31:03 -0300

    Feat: example main  if

--- 12-Estruturas_Controle/main.go
+++ 12-Estruturas_Controle/main.go
@@ -6,4 +6,10 @@ func main() {
 	fmt.Println("Estrutura de controle")
 	numberExample := 10
 	fmt.Println(numberExample)
+
+	if numberExample > 15 {
+		fmt.Println("Maior que 15")
+	} else {
+		fmt.Println("menos do que 15")
+	}
 }
