306c2c5124635a48be9f7f505cb125cc9c6ea116
julio_filizzola <juliofilizzola@hotmail.com>
2023-01-25 16:59:08 -0300

    feat: Final example

--- 21-Func-Closure/main.go
+++ 21-Func-Closure/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import "fmt"
 
+func closure() func() {
+	text := "Dentro da função clousre"
+
+	funcao := func() {
+		fmt.Println(text)
+	}
+
+	return funcao
+}
+
+func main() {
+	text := "dentro da função main"
+
+	fmt.Println(text)
+
+	funcNew := closure()
+	funcNew()
 }
