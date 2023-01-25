46a075ad571e14edbc46f8182dd120693e261fee
julio_filizzola <juliofilizzola@hotmail.com>
2023-01-25 10:38:53 -0300

    Feat: exemple func returns

--- 19-Defer/main.go
+++ 19-Defer/main.go
@@ -10,8 +10,18 @@ func func2() {
 	fmt.Println("Exec Func 2")
 }
 
+func validReciped(n1, n2 float32) bool {
+	fmt.Println("validando...")
+
+	media := (n1 + n2) / 2
+	fmt.Println(media)
+	return !!(media >= 6)
+}
+
 func main() {
 	defer func1()
 	//defe == adiar
+
+	defer fmt.Println(validReciped(23.2, 2))
 	func2()
 }
