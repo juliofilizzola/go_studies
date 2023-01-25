a10b7e0b2e8d639c2782d36fdaff15e20f40789e
julio_filizzola <juliofilizzola@hotmail.com>
2023-01-25 15:18:00 -0300

    feat: add panic from example
    Example valid panic message

--- 20-Panic-And-Recover/main.go
+++ 20-Panic-And-Recover/main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+func valid(n1, n2 float32) bool {
+	media := (n1 + n2) / 2
+
+	if media > 6 {
+		return true
+	} else if media < 6 {
+		return false
+	}
+
+	panic("A MÉIDA É EXATAMENTE 6!")
+}
+
 func main() {
 	fmt.Println("Panic and Recover")
+
+	fmt.Println(valid(6, 6))
 }
