76c86dd413a3201c94e9dbba9b7dc3f58dddd182
julio_filizzola <juliofilizzola@hotmail.com>
2023-01-26 17:39:09 -0300

    Feat: exemple func point

--- 22-Func-Pointer/main.go
+++ 22-Func-Pointer/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+func invert(nub int) int {
+	return nub * -1
+}
+func invertPoint(nub *int) {
+	*nub = *nub * -1
+}
+func main() {
+	number := 20
+	numberInvert := invert(number)
+	fmt.Println(number, "number original")
+	fmt.Println(numberInvert, "number invert")
+	newNumber := 40
+	fmt.Println(newNumber, "New number")
+	invertPoint(&newNumber)
+	fmt.Println(newNumber, "new Number invert point")
 }
