c3d9b9b5e9eb74352e9534a5bebf3965cf2a0096
julio_filizzola <juliofilizzola@hotmail.com>
2023-01-26 18:19:10 -0300

    Feat: finaly example methods

--- 24-Methods/main.go
+++ 24-Methods/main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+type user struct {
+	name string
+	age  uint8
+}
+
+func (u user) save(users user) {
+	fmt.Println("save")
+	fmt.Println(users)
+	fmt.Printf("Salva dada user %s in memory", u.name)
+}
+
 func main() {
 	fmt.Println("Methods")
+	userF := user{"julio", 26}
+	userF.save(userF)
 }
