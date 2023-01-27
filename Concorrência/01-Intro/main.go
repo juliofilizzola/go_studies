3dc04c2e5ae394e8201b832d72f212a88fe2c6eb
julio_filizzola <juliofilizzola@hotmail.com>
2023-01-27 16:24:42 -0300

    Feat: create loop, from example from gorountimes

--- Concorrência/01-Intro/main.go
+++ Concorrência/01-Intro/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func main() {
+	go escrever("xablau")
+	escrever("Hello word")
+}
+
+func escrever(text string) {
+	for {
+		fmt.Println(text)
+		time.Sleep(time.Second)
+	}
 }
