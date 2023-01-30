0b82394452daf28c2414675b983d7585c6e1fa3e
julio_filizzola <juliofilizzola@hotmail.com>
2023-01-30 18:27:42 -0300

    feat: example for 03-Channels

--- Concorrência/03-Channels/main.go
+++ Concorrência/03-Channels/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func main() {
+	channel := make(chan string)
+	go white("Hello", channel)
+	for {
+		message, open := <-channel
+		if !open {
+			break
+		}
+		fmt.Println(message)
+	}
+}
+
+func white(text string, channel chan string) {
+	for i := 0; i < 5; i++ {
+		channel <- text
+		time.Sleep(time.Second)
+	}
+
+	close(channel)
 }
