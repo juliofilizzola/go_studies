281f9cfb8683e53855c3495b02456c20134c5342
julio_filizzola <juliofilizzola@hotmail.com>
2023-02-02 08:00:06 -0300

    Feat: example from select intro

--- Concorrência/05-Select/main.go
+++ Concorrência/05-Select/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	fmt.Println("Select example")
+	channel1, channel2 := make(chan string), make(chan string)
+	go func() {
+		for {
+			time.Sleep(time.Millisecond * 500)
+			channel1 <- "channel 1"
+		}
+	}()
+
+	go func() {
+		for {
+			time.Sleep(time.Second * 2)
+			channel2 <- "channel 2"
+		}
+	}()
+
+	for {
+		messageChannel1 := <-channel1
+		fmt.Println(messageChannel1)
+
+		messageChannel2 := <-channel2
+		fmt.Println(messageChannel2)
+	}
 }
