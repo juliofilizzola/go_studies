e2e4c3d2e4084ce86ec36a6066165a453d039598
julio_filizzola <juliofilizzola@hotmail.com>
2023-02-05 12:39:47 -0300

    feat: final func worker

--- Concorrência/06-Worker-Pools/main.go
+++ Concorrência/06-Worker-Pools/main.go
@@ -4,6 +4,31 @@ import "fmt"
 
 func main() {
 	fmt.Println("Worker-Pools")
+	task := make(chan int, 45)
+	results := make(chan int, 45)
+
+	go worker(task, results)
+	go worker(task, results)
+	go worker(task, results)
+	go worker(task, results)
+
+	for index := 0; index < 45; index += 1 {
+		task <- index
+	}
+
+	close(task)
+
+	for index := 0; index < 45; index += 1 {
+		result := <-results
+		fmt.Println(result)
+	}
+
+}
+
+func worker(tasks <-chan int, result chan<- int) {
+	for task := range tasks {
+		result <- fibonacci(task)
+	}
 }
 
 func fibonacci(position int) int {
