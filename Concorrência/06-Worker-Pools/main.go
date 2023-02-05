6e65ffdd9fde97f92950675c73c8c6bb50ce260c
julio_filizzola <juliofilizzola@hotmail.com>
2023-02-05 12:26:49 -0300

    feat: create func fibonacci

--- Concorrência/06-Worker-Pools/main.go
+++ Concorrência/06-Worker-Pools/main.go
@@ -5,3 +5,11 @@ import "fmt"
 func main() {
 	fmt.Println("Worker-Pools")
 }
+
+func fibonacci(position int) int {
+	if position <= 1 {
+		return position
+	}
+
+	return fibonacci(position-2) + fibonacci(position-1)
+}
