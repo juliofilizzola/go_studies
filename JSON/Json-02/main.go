9a677fe8b0eb26b18547781db908433645879d2e
julio_filizzola <juliofilizzola@hotmail.com>
2023-02-18 19:44:48 -0300

    Feat: create example from unmarshal

--- JSON/Json-02/main.go
+++ JSON/Json-02/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
 
+type user struct {
+	Name     string `json:"name"`
+	LastName string `json:"last_name"`
+	Age      uint   `json:"age"`
+}
+
+func main() {
+	fmt.Println("UnMarshal")
+	user1 := `{"name": "julio", "age": 26, "last_name": "Fili"}`
+	u := user{}
+
+	if erro := json.Unmarshal([]byte(user1), &u); erro != nil {
+		log.Fatal(erro)
+	}
+
+	fmt.Println(u)
 }
