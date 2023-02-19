16f0036794b13f1aabb9405d02b9c648bfc9fe45
julio_filizzola <juliofilizzola@hotmail.com>
2023-02-19 19:07:13 -0300

    Feat: create module routes

--- HTTP/HTTP/main.go
+++ HTTP/HTTP/main.go
@@ -1,16 +1,15 @@
 package main
 
 import (
+	"HTTP/services/routes"
 	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Println("request feita")
-		writer.Write([]byte("Hello World"))
-	})
+	http.HandleFunc("/", routes.Home)
+	http.HandleFunc("/user", routes.User)
 	fmt.Println("Server started at port 8080")
 	log.Fatal(http.ListenAndServe(":5000", nil))
 }
