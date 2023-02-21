9f0ac7a889c62091bb99c1f28724e5c86a180a9c
julio_filizzola <juliofilizzola@hotmail.com>
2023-02-21 17:12:19 -0300

    feat: render html from request get

--- HTTP/HTTP_2/http/Request/Route.go
+++ HTTP/HTTP_2/http/Request/Route.go
@@ -1,9 +1,15 @@
 package route
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
+
+var templetes *template.Template
 
 func Route() {
+	templetes = template.Must(template.ParseGlob("./HTML/*.html"))
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-
+		templetes.ExecuteTemplate(writer, "index.html", nil)
 	})
 }
--- HTTP/HTTP_2/main.go
+++ HTTP/HTTP_2/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	route "HTTP_2/http/Request"
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func main() {
+	route.Route()
+	fmt.Println("Running server")
+	log.Fatal(http.ListenAndServe(":5000", nil))
 }
