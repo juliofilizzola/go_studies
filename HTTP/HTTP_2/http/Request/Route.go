b8901c36f79c60570edad0d8155dd129fa02fa7d
julio_filizzola <juliofilizzola@hotmail.com>
2023-02-21 18:56:32 -0300

    feat: render html add name and email

--- HTTP/HTTP_2/http/Request/Route.go
+++ HTTP/HTTP_2/http/Request/Route.go
@@ -7,9 +7,25 @@ import (
 
 var templetes *template.Template
 
+type user struct {
+	Name  string
+	Email string
+}
+
 func Route() {
 	templetes = template.Must(template.ParseGlob("./HTML/*.html"))
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
 		templetes.ExecuteTemplate(writer, "index.html", nil)
+	})
+}
+
+func Route2() {
+	templetes = template.Must(template.ParseGlob("./HTML/*.html"))
+	http.HandleFunc("/user", func(writer http.ResponseWriter, request *http.Request) {
+		user := user{
+			Name:  "julio",
+			Email: "juliofilizzola@hotmail.com",
+		}
+		templetes.ExecuteTemplate(writer, "index2.html", user)
 	})
 }
--- HTTP/HTTP_2/main.go
+++ HTTP/HTTP_2/main.go
@@ -9,6 +9,7 @@ import (
 
 func main() {
 	route.Route()
+	route.Route2()
 	fmt.Println("Running server")
 	log.Fatal(http.ListenAndServe(":5000", nil))
 }
