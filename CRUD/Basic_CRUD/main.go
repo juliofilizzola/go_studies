4de7be7a18f5cf4acc5241db810b5147f4b94d13
julio_filizzola <juliofilizzola@hotmail.com>
2023-02-24 14:20:39 -0300

    FEAT: create route

--- CRUD/Basic_CRUD/main.go
+++ CRUD/Basic_CRUD/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("BASICO DE CRUD AQUI...")
+	route := mux.NewRouter()
+
+	fmt.Println("Server running...")
+	log.Fatal(http.ListenAndServe(":5000", route))
 }
